ef6ec3c1ec0b799c6398414456570c77284444d4
Len <villalobosrojas22@gmail.com>
2024-07-26 22:55:05 -0600

    Set volume options

--- js/controllers/optionsController/optionView.js
+++ js/controllers/optionsController/optionView.js
@@ -1,3 +1,4 @@
+import { successAlert } from "../../librerias/alerts.js";
 import { BLUE, GREEN, PINK, RED } from "../../librerias/constantes.js";
 import { div, p } from "../../librerias/html.js";
 import { ViewEnhanced } from "../../views/viewEnhanced.js";
@@ -9,17 +10,40 @@ export class OptionView extends ViewEnhanced {
     constructor(controller, parent) {
         super(controller, parent);
         this.container.className = 'optionController';
-
         this.returnController = new ReturnController(this, this.container)
+
+        /* character container */
         this.characterContainer = div(this.elementsContainer, { className: "characterContainerOpt" })
         this.character = new CharacterController(this, this.characterContainer);
 
+        /* Change color */
         this.changeColorText = p(this.elementsContainer, { className: 'changeColorTitle', textContent: 'Change color' });
         this.selectColorContainer = div(this.elementsContainer, { className: 'selectContainer' });
         this.red = div(this.selectColorContainer, { id: 'red', className: 'btnColor', onclick: this.onChangeToRed.bind(this) });
         this.green = div(this.selectColorContainer, { id: 'green', className: 'btnColor', onclick: this.onChangeToGreen.bind(this) });
         this.blue = div(this.selectColorContainer, { id: 'blue', className: 'btnColor', onclick: this.onChangeToBlue.bind(this) });
         this.pink = div(this.selectColorContainer, { id: 'pink', className: 'btnColor', onclick: this.onChangeToPink.bind(this) });
+
+        /* Set volume */
+        this.volumeTitle = p(this.elementsContainer, { className: 'volumeTitle', textContent: 'Set the volume' });
+        this.setVolumeContainer = div(this.elementsContainer, { className: 'setVolumeContainer' });
+
+        this.volumeIcon = p(this.setVolumeContainer, { className: 'volumeIcon', textContent: '🕪' });
+        this.barsVolumeContainer = div(this.setVolumeContainer, { className: 'barsVolumeContainer' })
+
+        this.firstBar = div(this.barsVolumeContainer, { className: 'volumeBars' });
+        this.secondBar = div(this.barsVolumeContainer, { className: 'volumeBars' });
+        this.thirdBar = div(this.barsVolumeContainer, { className: 'volumeBars' });
+        this.fourthBar = div(this.barsVolumeContainer, { className: 'volumeBars' });
+        this.fifthBar = div(this.barsVolumeContainer, { className: 'volumeBars' });
+        this.sixthBar = div(this.barsVolumeContainer, { className: 'volumeBars' });
+        this.seventhBar = div(this.barsVolumeContainer, { className: 'volumeBars' });
+
+        /* log out */
+
+        this.btnsContainer = div(this.elementsContainer, { className: 'btnsContainer' })
+        this.creditsBtn = div(this.btnsContainer, { className: 'menuBtns', innerHTML: 'Credits' });
+        this.logBtn = div(this.btnsContainer, { className: 'menuBtns', innerHTML: 'Login' });
     }
 
     onChangeToRed() {
